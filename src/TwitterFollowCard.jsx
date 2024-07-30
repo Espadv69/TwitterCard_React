f6ae2aa33498b08df04be126a207a07fa48c488c
espadv69 <aitorjob69@gmail.com>
2024-07-30 21:24:51 +0200

    Update: hide mesagges to guide user

--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,6 +1,8 @@
-export function TwitterFollowCard ({ formatUserName, username, name, isFollowing }) {
-    console.log(isFollowing)
-    const imageSrc = `https://unavatar.io/${username}`
+// Se exporta esta función 👇 a App.jsx
+export function TwitterFollowCard ({ formatUserName, username, name, isFollowing }) { // 👈 Esta función recoge 4 parámetros
+    console.log(isFollowing) // 👈 Aquí muestra en consola a los que se sigue en Boolean
+    const imageSrc = `https://unavatar.io/${username}` // 👈 Aquí se crea una variable para recoger las imágenes que tenga el username
+    // 👇 Aquí retornamos la función para que se renderice
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
