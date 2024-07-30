3887758b8addb59a83702e6617c9775d7d9924e8
espadv69 <aitorjob69@gmail.com>
2024-07-30 20:16:12 +0200

    Update: App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import './App.css'
 export function App () {
     return (
-        <article>
-            <header>
-                <img src="https://unavatar.io/midudev" alt="avatar de midudev" />
-                <div>
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img
+                    className='tw-followCard-avatar'
+                    src="https://unavatar.io/midudev"
+                    alt="avatar de midudev" />
+                <div className='tw-followCard-div'>
                     <strong>Miguel Ángel Durán</strong>
-                    <span>@midudev</span>
+                    <span className='tw-followCard-span'>@midudev</span>
                 </div>
             </header>
             <aside>
-                <button>
+                <button
+                    className='tw-followCard-button'
+                >
                     Seguir
                 </button>
             </aside>
